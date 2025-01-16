8b586d7298135eda3b17c54a11f8602d9d49dc25
Joe Griffith <joeagriffith@gmail.com>
2025-01-16 12:02:59 +0000

    built schedulers

---
+++ optim/__init__.py
@@ -0,0 +1,3 @@
+from optim.schedule import cosine_schedule, step_schedule, flat_schedule
+
+__all__ = ['cosine_schedule', 'step_schedule', 'flat_schedule']
--- optim/utils.py
+++ optim/utils.py
@@ -1,41 +1,6 @@
 import torch
 import math
 
-def cosine_schedule(base, end, T):
-    return end - (end - base) * ((torch.arange(0, T, 1) * math.pi / T).cos() + 1) / 2
-
-def build_schedule(T, base, end=None, schedule='flat', warmup=0, milestones=None, gamma=None):
-    # Warmup phase
-    warmup_vals = torch.linspace(0.0, base, warmup+1)[1:] if warmup > 0 else torch.tensor([])
-
-    # Decay phase
-    if T < warmup:
-        decay_vals = torch.tensor([])
-    else:
-        if schedule == 'flat':
-            decay_vals = torch.ones(T - warmup) * base
-
-        elif schedule == 'cosine':
-            assert end is not None, '"end: float" must be provided for cosine schedule'
-            decay_vals = cosine_schedule(base, end, T - warmup)
-
-        elif schedule == 'step':
-            assert milestones is not None, '"milestones: list[int]" must be provided for step schedule'
-            assert gamma is not None, '"gamma: float" must be provided for step schedule'
-            decay_vals = torch.tensor([])
-            t = warmup
-            val = base
-            for milestone in milestones:
-                decay_vals = torch.cat([decay_vals, torch.ones(milestone - t) * val])
-                t = milestone
-                val = val * gamma
-            if t < T:
-                decay_vals = torch.cat([decay_vals, torch.ones(T - t) * val])
-
-        else:
-            raise ValueError(f'Unknown learning rate schedule: {schedule}')
-
-    return torch.cat([warmup_vals, decay_vals])[:T]
 
 
 def get_optimiser(model, optim='AdamW', betas=(0.9, 0.999), momentum=0.9, lr=99999.9, wd=99999.9):

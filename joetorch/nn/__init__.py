9b3cf0284e31162ef9b9aebf80775b60858ba7b1
Joe Griffith <joeagriffith@gmail.com>
2025-04-20 13:24:27 +0000

    bugfix

--- joetorch/nn/__init__.py
+++ joetorch/nn/__init__.py
@@ -3,4 +3,4 @@ from joetorch.nn.mnist import *
 from joetorch.nn.convmixer import *
 from joetorch.nn.stable_diffusion import *
 
-__all__ = ['MLP', 'EncBlock', 'DecBlock', 'ConvResidualBlock', 'ConvMixer', 'SelfAttention', 'CrossAttention','LargeEncoder', 'MediumEncoder', 'SmallEncoder', 'LargeDecoder', 'MediumDecoder', 'SmallDecoder', 'MNIST_MLP_Encoder', 'MNIST_MLP_Decoder', 'MNIST_CNN_Encoder', 'MNIST_CNN_Decoder', 'MNIST_AE']
+__all__ = ['MLP', 'EncBlock', 'DecBlock', 'ConvResidualBlock', 'convmixer', 'SelfAttention', 'CrossAttention','LargeEncoder', 'MediumEncoder', 'SmallEncoder', 'LargeDecoder', 'MediumDecoder', 'SmallDecoder', 'MNIST_MLP_Encoder', 'MNIST_MLP_Decoder', 'MNIST_CNN_Encoder', 'MNIST_CNN_Decoder', 'MNIST_AE']
